5d35fbd8089f796fd933562bfce8b235fcefc2a5
JeremyCraigMartinez <jeremymartinez11@gmail.com>
2018-03-11 14:51:24 -0700

    move clustering helpers to helpers file
    linting changes

--- src/helpers/clustering.py
+++ src/helpers/clustering.py
@@ -1,24 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 16 10:47:56 2017
-
-@author: jtay
-"""
-
-import numpy as np
 from collections import Counter
+import numpy as np
 from sklearn.metrics import accuracy_score as acc
 from sklearn.mixture import GaussianMixture as GMM
 from sklearn.metrics.pairwise import pairwise_distances
 from sklearn.feature_selection import mutual_info_classif as MIC
-from sklearn.base import TransformerMixin,BaseEstimator
+from sklearn.base import TransformerMixin, BaseEstimator
 import scipy.sparse as sps
 from scipy.linalg import pinv
 
-nn_arch= [(50,50),(50,),(25,),(25,25),(100,25,100)]
-nn_reg = [10**-x for x in range(1,5)]
+nn_arch = [(50, 50), (50, ), (25, ), (25, 25), (100, 25, 100)]
+nn_reg = [10**-x for x in range(1, 5)]
 
-def cluster_acc(Y,clusterLabels):
+def cluster_acc(Y, clusterLabels):
     assert (Y.shape == clusterLabels.shape)
     pred = np.empty_like(Y)
     for label in set(clusterLabels):
@@ -27,29 +20,25 @@ def cluster_acc(Y,clusterLabels):
         target = Counter(sub).most_common(1)[0][0]
         pred[mask] = target
 #    assert max(pred) == max(Y)
-#    assert min(pred) == min(Y)    
-    return acc(Y,pred)
-
+#    assert min(pred) == min(Y)
+    return acc(Y, pred)
 
 class myGMM(GMM):
-    def transform(self,X):
+    def transform(self, X):
         return self.predict_proba(X)
-        
-        
-def pairwiseDistCorr(X1,X2):
+
+def pairwiseDistCorr(X1, X2):
     assert X1.shape[0] == X2.shape[0]
-    
+
     d1 = pairwise_distances(X1)
     d2 = pairwise_distances(X2)
-    return np.corrcoef(d1.ravel(),d2.ravel())[0,1]
+    return np.corrcoef(d1.ravel(), d2.ravel())[0, 1]
 
-    
-def aveMI(X,Y):    
-    MI = MIC(X,Y) 
+def aveMI(X, Y):
+    MI = MIC(X, Y)
     return np.nanmean(MI)
-    
-  
-def reconstructionError(projections,X):
+
+def reconstructionError(projections, X):
     W = projections.components_
     if sps.issparse(W):
         W = W.todense()
@@ -57,23 +46,21 @@ def reconstructionError(projections,X):
     reconstructed = ((p@W)@(X.T)).T # Unproject projected data
     errors = np.square(X-reconstructed)
     return np.nanmean(errors)
-    
-    
-        
-# http://datascience.stackexchange.com/questions/6683/feature-selection-using-feature-importances-in-random-forests-with-scikit-learn          
+
+# http://datascience.stackexchange.com/questions/6683/feature-selection-using-feature-importances-in-random-forests-with-scikit-learn
 class ImportanceSelect(BaseEstimator, TransformerMixin):
     def __init__(self, model, n=1):
-         self.model = model
-         self.n = n
+        self.model = model
+        self.n = n
     def fit(self, *args, **kwargs):
-         self.model.fit(*args, **kwargs)
-         return self
+        self.model.fit(*args, **kwargs)
+        return self
     def transform(self, X):
-         return X[:,self.model.feature_importances_.argsort()[::-1][:self.n]]
-                  
+        return X[:, self.model.feature_importances_.argsort()[::-1][:self.n]]
+
 #http://stats.stackexchange.com/questions/90769/using-bic-to-estimate-the-number-of-k-in-kmeans
-from scipy.spatial import distance     
-def compute_bic(kmeans,X):
+from scipy.spatial import distance
+def compute_bic(kmeans, X):
     """
     Computes the BIC metric for a given clusters
 
@@ -89,7 +76,7 @@ def compute_bic(kmeans,X):
     """
     # assign centers and labels
     centers = [kmeans.cluster_centers_]
-    labels  = kmeans.labels_
+    labels = kmeans.labels_
     #number of clusters
     m = kmeans.n_clusters
     # size of the clusters
@@ -102,9 +89,6 @@ def compute_bic(kmeans,X):
 
     const_term = 0.5 * m * np.log(N) * (d+1)
 
-    BIC = np.sum([n[i] * np.log(n[i]) -
-               n[i] * np.log(N) -
-             ((n[i] * d) / 2) * np.log(2*np.pi*cl_var) -
-             ((n[i] - 1) * d/ 2) for i in range(m)]) - const_term
+    BIC = np.sum([n[i] * np.log(n[i]) - n[i] * np.log(N) - ((n[i] * d) / 2) * np.log(2*np.pi*cl_var) - ((n[i] - 1) * d/ 2) for i in range(m)]) - const_term
 
-    return(BIC)                  
+    return(BIC)
