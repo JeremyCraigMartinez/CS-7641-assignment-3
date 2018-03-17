7acb841705404dc6057b6b45691fea42b67c0fcb
JeremyCraigMartinez <jeremymartinez11@gmail.com>
2018-03-17 01:00:13 -0700

    add compare function to bic
    allow to compare best across different dimensionalit
    reduction algorithms to see which n_components yields
    best EM cluster (according to BIC)

--- src/bic.py
+++ src/bic.py
@@ -17,8 +17,9 @@ print(__doc__)
 
 dir_path = dirname(realpath(__file__))
 output_dir = sys.argv[1] if len(sys.argv) >= 2 else 'BASE'
-OUT = '{}/../OUTPUT/{}'.format(dir_path, output_dir)
-BASE = '{}/../OUTPUT/BASE'.format(dir_path)
+OUTPUT = '{}/../OUTPUT'.format(dir_path)
+OUT = '{}/{}'.format(OUTPUT, output_dir)
+BASE = '{}/BASE'.format(OUTPUT)
 
 # Generating the sample data from make_blobs
 # This particular setting has one distinct cluster and 3 clusters placed close
@@ -31,10 +32,10 @@ r_components = [8, 13, 21, 34, 55, 89, 104, 119, 134, 159]
 c_components = [8, 10, 14, 18, 25, 35, 45, 55, 65, 75]
 n_components_range = r_components
 
-def main(X, _):
-    lowest_bic = np.infty
-    bic = []
+def find_best(X, bic):
     cv_types = ['spherical', 'tied', 'diag', 'full']
+    lowest_bic = np.infty
+
     for cv_type in cv_types:
         for n_components in n_components_range:
             # Fit a Gaussian mixture with EM
@@ -45,16 +46,23 @@ def main(X, _):
             if bic[-1] < lowest_bic:
                 lowest_bic = bic[-1]
                 best_gmm = gmm
+    return best_gmm
+
+def main(X, _):
+    bic = []
+    find_best(X, bic)
 
     bic = np.array(bic)
+
+def plot_gmms(bic):
+    cv_types = ['spherical', 'tied', 'diag', 'full']
     color_iter = itertools.cycle(['navy', 'turquoise', 'cornflowerblue',
                                   'darkorange'])
-    clf = best_gmm
     bars = []
 
     # Plot the BIC scores
     spl = plt.subplot(2, 1, 1)
-    for i, (cv_type, color) in enumerate(zip(cv_types, color_iter)):
+    for i, (_, color) in enumerate(zip(cv_types, color_iter)):
         xpos = np.array(n_components_range) + 1.7 * (i - 2)
         bars.append(plt.bar(xpos, bic[i * len(n_components_range):
                                       (i + 1) * len(n_components_range)],
@@ -70,28 +78,30 @@ def main(X, _):
 
     # Plot the winner
     splot = plt.subplot(2, 1, 2) # pylint: disable=W0612
-    #Y_ = clf.predict(X)
-    #for i, (mean, cov, color) in enumerate(zip(clf.means_, clf.covariances_,
-    #                                           color_iter)):
-    #    print(cov)
-    #    v, w = linalg.eigh(cov)
-    #    if not np.any(Y_ == i):
-    #        continue
-    #    plt.scatter(X[Y_ == i, 0], X[Y_ == i, 1], .8, color=color)
-    #    # Plot an ellipse to show the Gaussian component
-    #    angle = np.arctan2(w[0][1], w[0][0])
-    #    angle = 180. * angle / np.pi  # convert to degrees
-    #    v = 2. * np.sqrt(2.) * np.sqrt(v)
-    #    ell = mpl.patches.Ellipse(mean, v[0], v[1], 180. + angle, color=color)
-    #    ell.set_clip_box(splot.bbox)
-    #    ell.set_alpha(.5)
-    #    splot.add_artist(ell)
 
     plt.xticks(())
     plt.yticks(())
     plt.title('Selected GMM: full model, 2 components')
     plt.subplots_adjust(hspace=.35, bottom=.02)
     plt.show()
+
+def compare(dim_red_alg):
+    global BASE
+    r_bests = []
+    c_bests = []
+    if dim_red_alg == 'PCA':
+        params = ['0.6-', '0.7-', '0.8-', '0.9-', '0.95-']
+        BASE = '{}/PCA'.format(OUTPUT)
+
+    for param in params:
+        _r, _c = get_data(BASE, param)
+        r, _ = _r
+        c, _ = _c
+        r_bests.append(find_best(r, []))
+        c_bests.append(find_best(c, []))
+
+    plot_gmms(r_bests)
+    plot_gmms(c_bests)
 
 def plot_results(X, Y_, means, covariances, index, title):
     color_iter = itertools.cycle(['navy', 'c', 'cornflowerblue', 'gold', 'darkorange'])
@@ -138,6 +148,10 @@ def epsilloids(X, n):
     plt.show()
 
 if __name__ == '__main__':
-    #main(r_X, r_y)
-    #main(c_X, c_y)
-    epsilloids(r_X, 55)
+    if '--compare' in sys.argv:
+        compare('PCA')
+    elif 'epsilloids' in sys.argv:
+        epsilloids(r_X, 55)
+    else:
+        main(r_X, r_y)
+        main(c_X, c_y)
